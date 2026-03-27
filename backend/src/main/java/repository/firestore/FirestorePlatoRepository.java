854e5e15767e63dd202d8194dc12d01d0aa0716c
Joan <drdoffensmirhtz@gmail.com>
2026-03-27 15:49:36 +0000

    created folders for interfaces and firetore inside repository

---
+++ backend/src/main/java/repository/firestore/FirestorePlatoRepository.java
@@ -0,0 +1,4 @@
+package repository.firestore;
+
+public class FirestorePlatoRepository {
+}
--- backend/src/main/java/repository/interfaces/Repository.java
+++ backend/src/main/java/repository/interfaces/Repository.java
@@ -1,4 +1,4 @@
-package repository;
+package repository.interfaces;
 
 import java.util.List;
 import java.util.Optional;
